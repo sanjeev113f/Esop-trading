817031410baf4b3336395922f3968327684102d9
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-01 15:37:53 +0530

    Update deployment scrupt

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 value=$(sudo lsof -n -i :80 | grep LISTEN | awk -F ' ' '{print $2}') && sudo kill -9 $value > /dev/null
+
 if [-z $value]
 then
-  sudo nohup MICRONAUT_SERVER_PORT=80 java -jar esop-0.1-all.jar  > /dev/null &
+  sudo MICRONAUT_SERVER_PORT=80 nohup java -jar esop-0.1-all.jar  > /dev/null &
   exit
 else
   sudo kill -9 $value
-  sudo nohup MICRONAUT_SERVER_PORT=80 java -jar esop-0.1-all.jar > /dev/null &
+  sudo MICRONAUT_SERVER_PORT=80 nohup java -jar esop-0.1-all.jar > /dev/null &
   exit
 fi
