b252073edd8c48e2d46619f0325bb29ca54fdf36
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-01 15:27:42 +0530

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 value=$(sudo lsof -n -i :80 | grep LISTEN | awk -F ' ' '{print $2}') && sudo kill -9 $value > /dev/null
-MICRONAUT_SERVER_PORT=80
 if [-z $value]
 then
-  sudo nohup java -jar esop-0.1-all.jar  > /dev/null &
+  sudo nohup MICRONAUT_SERVER_PORT=80 java -jar esop-0.1-all.jar  > /dev/null &
   exit
 else
   sudo kill -9 $value
-  sudo nohup java -jar esop-0.1-all.jar > /dev/null &
+  sudo nohup MICRONAUT_SERVER_PORT=80 java -jar esop-0.1-all.jar > /dev/null &
   exit
 fi
