8c4c1b96c19bba73bcac499d4741fab334813d53
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-01 15:22:30 +0530

    Changed micronaut server port to 80

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 value=$(sudo lsof -n -i :80 | grep LISTEN | awk -F ' ' '{print $2}') && sudo kill -9 $value > /dev/null
-MICRONAUT_SERVER_PORT=7070
+MICRONAUT_SERVER_PORT=80
 if [-z $value]
 then
   sudo nohup java -jar esop-0.1-all.jar  > /dev/null &
