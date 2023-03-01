6f025d29aa43bacbf5569c2328a3164eae45f6c9
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-01 15:13:58 +0530

    Change

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 value=$(sudo lsof -n -i :80 | grep LISTEN | awk -F ' ' '{print $2}') && sudo kill -9 $value > /dev/null
 if [-z $value]
 then
-  sudo nohup java -jar m2.jar  > /dev/null &
+  sudo nohup java -jar esop-0.1-all.jar  > /dev/null &
   exit
 else
   sudo kill -9 $value
-  sudo nohup java -jar m2.jar > /dev/null &
+  sudo nohup java -jar esop-0.1-all.jar > /dev/null &
   exit
 fi
