2a4bcc28d5023a496937350d0909dac40dfaa24e
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-24 11:52:35 +0530

    Change terraform file

--- main.tf
+++ main.tf
@@ -42,12 +42,18 @@ resource "aws_security_group" "gurukul_2023_security_group" {
   description = "Allow SSH and HTTP traffic from all sources"
 
   ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     protocol    = "tcp"
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port        = 0
     to_port          = 0
