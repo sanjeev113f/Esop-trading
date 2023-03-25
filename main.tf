559c341f70c00dbc5978125e7cef6669527b2966
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-25 10:44:05 +0530

    Change terraform command

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ resource "aws_instance" "app_server" {
   instance_type               = "t2.micro"
   key_name                    = data.aws_key_pair.gurukul-2023.key_name
   associate_public_ip_address = true
-  subnet_id = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.gurukul_2023_security_group.id]
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.gurukul_2023_security_group.id]
 
   tags = {
     Name      = "gurukul-sanjeev"
@@ -40,7 +40,7 @@ resource "aws_instance" "app_server" {
 
 resource "aws_security_group" "gurukul_2023_security_group" {
   name        = "allow_ssh"
-  vpc_id        = "vpc-019c09a1a0c5b4f6b"
+  vpc_id      = "vpc-019c09a1a0c5b4f6b"
   description = "Allow SSH and HTTP traffic from all sources"
 
   ingress {
@@ -75,6 +75,7 @@ resource "aws_subnet" "main" {
 output "instance_ip_addr" {
   value = aws_instance.app_server.public_ip
 }
+
 
 output "instance_id" {
   description = "ID of the EC2 instance"
